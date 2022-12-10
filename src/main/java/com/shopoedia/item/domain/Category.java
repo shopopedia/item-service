e09f1c9dbf61048ba57a65267b778f32f57bbd0e
parishkar-nagrikar <p4parish@gmail.com>
2022-12-10 21:41:00 +0530

    error resolved

--- src/main/java/com/shopoedia/item/domain/Category.java
+++ src/main/java/com/shopoedia/item/domain/Category.java
@@ -3,6 +3,7 @@ package com.shopoedia.item.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -13,8 +14,8 @@ import org.hibernate.annotations.GenericGenerator;
 @Table(uniqueConstraints={@UniqueConstraint(columnNames={"name"})})
 public class Category {
 
-    @Id @GeneratedValue(generator="system-uuid")
-    @GenericGenerator(name="system-uuid", strategy = "")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
 
     @Column(name = "name")
--- src/main/java/com/shopoedia/item/domain/Product.java
+++ src/main/java/com/shopoedia/item/domain/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Product {
+	
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
